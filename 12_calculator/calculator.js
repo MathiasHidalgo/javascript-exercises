233d1583861466a9cb933c92b2de348d605c7649
Mathias Hidalgo <mejhc2307lbc@gmail.com>
2025-05-12 14:02:21 -0500

    Calculator Done! still need to finish the rest!

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(arg1, arg2) {
+	return arg1 + arg2;
 };
 
-const subtract = function() {
-	
+const subtract = function(arg1, arg2) {
+	return arg1 - arg2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if (array.length === 0) {
+    return 0
+  }
+  return array.reduce((acc, curr) => acc + curr)
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((acc, curr)=> acc * curr, 1)
 };
 
-const power = function() {
-	
+const power = function(arg1, arg2) {
+	return arg1 ** arg2;
 };
 
-const factorial = function() {
-	
+const factorial = function(arg) {
+	const array = []
+  for(i = 1; i <= arg ; i++){
+    array.push(i)
+  }
+  return array.reduce((acc, curr) => acc * curr, 1)
+
 };
 
 // Do not edit below this line
